66097629aad717ea615256056a33b91a7f56a3ee
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-05-06 01:24:11 +0300

    Updated the datepicker to use jQuery UI

--- LibrarySystem/Models/Book.cs
+++ LibrarySystem/Models/Book.cs
@@ -20,7 +20,6 @@ namespace LibrarySystem.Models
         public string Title { get; set; }
         public string Type { get; set; }
         public int Edition { get; set; }
-        [DataType(DataType.DateTime)]
         public System.DateTime ReleaseDate { get; set; }
         public int Writer { get; set; }
         public string Image { get; set; }
